6e92d48b3935442f599cfcf79a872b498d2b3855
Owen Smith <owen@omsmith.ca>
2014-04-10 13:14:51 -0400

    Sign tests

---
+++ spec/sign_spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var expect = require('chai').expect,
+	url = require('url');
+
+var D2L = require('../');
+
+describe('D2L.Util.Sign', function () {
+	it('should return base64Url\'d HMAC-SHA256', function (done) {
+		expect(D2L.Util.Sign('foobar', 'bar')).to.equal('8MWnO0rB4dhvQbYdde_vEx3dbLS3RgumO-9WT90kQnQ');
+		done();
+	});
+});

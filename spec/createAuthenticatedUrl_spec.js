35e02fd9184eeb6dfe5651f3ba0672eef8773209
Jacob Parker <jacob.parker@desire2learn.com>
2014-04-18 11:33:12 -0400

    (chore) fix whitespace in tests

--- spec/createAuthenticatedUrl_spec.js
+++ spec/createAuthenticatedUrl_spec.js
@@ -7,22 +7,22 @@ var D2L = require('../');
 
 describe('D2L.UserContext.createAuthenticatedUrl', function () {
 	var appContext;
-    var userContext;
+	var userContext;
 
 	before(function (done) {
 		appContext = new D2L.ApplicationContext('foo', 'bar');
 		userContext = appContext.createUserContextWithValues('http://somelms.edu', 80, 'baz', 'quux', 0);
-        done();
+		done();
 	});
 
 	it('should not include query params in signature', function (done) {
-        var urlWith = url.parse(userContext.createAuthenticatedUrl('/d2l/api/foo?bar=baz', 'GET'), true);
-        var urlWithout = url.parse(userContext.createAuthenticatedUrl('/d2l/api/foo', 'GET'), true);
+		var urlWith = url.parse(userContext.createAuthenticatedUrl('/d2l/api/foo?bar=baz', 'GET'), true);
+		var urlWithout = url.parse(userContext.createAuthenticatedUrl('/d2l/api/foo', 'GET'), true);
 
-        expect(urlWith.query['x_c']).to.equal(urlWithout.query['x_c']);
-        expect(urlWith.query['x_d']).to.equal(urlWithout.query['x_d']);
+		expect(urlWith.query['x_c']).to.equal(urlWithout.query['x_c']);
+		expect(urlWith.query['x_d']).to.equal(urlWithout.query['x_d']);
 
-	    done();
+		done();
 	});
 });
 

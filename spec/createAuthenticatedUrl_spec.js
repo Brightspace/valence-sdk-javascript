12ab99ff105a0ffaf6df4a347a5d5af6655291c6
Jacob Parker <jacob.parker@desire2learn.com>
2014-04-18 10:59:01 -0400

    (fix) Don't include query params in signatures

---
+++ spec/createAuthenticatedUrl_spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var expect = require('chai').expect,
+	url = require('url');
+
+var D2L = require('../');
+
+describe('D2L.UserContext.createAuthenticatedUrl', function () {
+	var appContext;
+    var userContext;
+
+	before(function (done) {
+		appContext = new D2L.ApplicationContext('foo', 'bar');
+		userContext = appContext.createUserContextWithValues('http://somelms.edu', 80, 'baz', 'quux', 0);
+        done();
+	});
+
+	it('should not include query params in signature', function (done) {
+        var urlWith = url.parse(userContext.createAuthenticatedUrl('/d2l/api/foo?bar=baz', 'GET'), true);
+        var urlWithout = url.parse(userContext.createAuthenticatedUrl('/d2l/api/foo', 'GET'), true);
+
+        expect(urlWith.query['x_c']).to.equal(urlWithout.query['x_c']);
+        expect(urlWith.query['x_d']).to.equal(urlWithout.query['x_d']);
+
+	    done();
+	});
+});
+
